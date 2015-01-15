39526b1eaf810c55aeffcbb2318c960f0f91f16a
Richard Green <richardagreen@gmail.com>
2015-01-15 19:12:47 +0000

    Update transactions.py

--- bc/bitcoin/transactions.py
+++ bc/bitcoin/transactions.py
@@ -4,6 +4,7 @@ import httplib
 
 def get_balance(pub_key):
     try:
+        # RG - this returns the balance in bitcoins of that address
         h1 = httplib.HTTPConnection("blockchain.info")
         h1.request("GET","/q/getreceivedbyaddress/%s" % (pub_key))
         r = h1.getresponse()
@@ -16,7 +17,6 @@ def get_last_transaction(pub_key):
     # return the amount of the last transaction
     pass
 
-def flush_funds(wallet):
-    #TODO
-    # move funds to central wallet
+def flush_funds(private_key, wallet):
+    # RG: move all funds from privatekey to wallet so that we don't double authorize.
     pass
